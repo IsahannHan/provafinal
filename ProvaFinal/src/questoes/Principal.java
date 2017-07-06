06ed3b2b4bd0edec915dc50aa5860d9c563924a1
IsahannHan <isahanacleto@gmail.com>
2017-07-06 19:51:54 -0300

    Classe principal para checagem das repostas

---
+++ ProvaFinal/src/questoes/Principal.java
@@ -0,0 +1,51 @@
+package questoes;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		int ra = 160381;
+		
+		System.out.println("Questão 1:");
+		Ex01.execute(ra);
+		System.out.println("\n");
+		
+		System.out.println("Questão 2:");
+		Ex02.execute(ra);
+		System.out.println("\n");
+		
+		System.out.println("Questão 3:");
+		Ex03.execute(ra);
+		System.out.println("\n");
+		
+		System.out.println("Questão 4:");
+		Ex04.execute(ra);
+		System.out.println("\n");
+		
+		System.out.println("Questão 5:");
+		Ex05.execute(ra);
+		System.out.println("\n");
+		
+		System.out.println("Questão 6:");
+		Ex06.execute(ra);
+		System.out.println("\n");
+		
+		System.out.println("Questão 7:");
+		Ex07.execute(ra);
+		System.out.println("\n");
+		
+		System.out.println("Questão 8:");
+		Ex08.execute(ra);
+		System.out.println("\n");
+		
+		System.out.println("Questão 9:");
+		Ex09.execute(ra);
+		System.out.println("\n");
+		
+		System.out.println("Questão 10:");
+		Ex10.execute(ra);
+		System.out.println("\n");
+		
+		
+	}
+
+}
