4c7306e37769226fad4fdc84e6981ec8f6a61835
IsahannHan <isahanacleto@gmail.com>
2017-07-06 19:29:39 -0300

    Questão 4

---
+++ ProvaFinal/src/questoes/Ex04.java
@@ -0,0 +1,29 @@
+package questoes;
+
+public class Ex04 {
+	private static char[] vet;
+	public static void execute(int num){
+		char[] charVet = String.valueOf(num).toCharArray();
+		vet = new char[charVet.length * 2];
+		
+		for(int i = 0; i < charVet.length; i++){
+			vet[i] = charVet[i];
+		}
+		
+		for(int i = 0; i < charVet.length; i++){
+			vet[i + charVet.length] = charVet[i];
+		}
+		
+		char[] result = new char[charVet.length / 2];
+		
+		for(int i = 0; i < result.length; i++){
+			result[i] = vet[i*2];
+		}
+		
+		for(char c : result){
+			System.out.println(c);
+		}
+		
+		System.out.println("");
+	}
+}
